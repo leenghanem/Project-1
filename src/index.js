d682896225f1fe9919aee55cca6488b4505041f8
leen ghanem <leen.w.ghanem@gmail.com>
2021-06-10 00:12:55 +0300

    'Project-1'

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
+import '@fortawesome/fontawesome-free/js/all.min';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/popper.min';
+
+console.log('hi');
